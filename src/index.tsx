510752f02e327339a5184ec9cff04110c8a5ba96
igar1991 <iiggaarr19912004@gmail.com>
2023-05-19 19:37:54 +0300

    feat: add warning alert

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,17 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
+import { Alert } from '@mui/material'
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 root.render(
   <BrowserRouter>
     <Provider store={store}>
+      <Alert severity="error">
+        This is a development version of the website. Please note that all articles and data will be removed upon the
+        release of the main version. Thank you for your understanding.
+      </Alert>
       <App />
     </Provider>
   </BrowserRouter>,
