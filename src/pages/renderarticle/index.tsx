58c8e1ed6e43853fcaff3816bd75420542576d9a
igar1991 <iiggaarr19912004@gmail.com>
2023-06-21 23:33:27 +0300

    refactor: render article

--- src/pages/renderarticle/index.tsx
+++ src/pages/renderarticle/index.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from 'react'
-import { Container } from '@mui/material'
+import { Box, Container, Skeleton, Typography } from '@mui/material'
 import Output from 'editorjs-react-renderer'
 import { useParams } from 'react-router-dom'
 import { getArticleById } from '../../api/users'
 import { SmallAvatar } from '../../components/smallAvatar'
 import { shortenString } from '../../utils'
+import { NotFoundComponent } from '../../components/notfound'
 
 interface Article {
   author_id: number
@@ -20,34 +21,52 @@ interface Article {
 export const RenderArticle = () => {
   const { articleId, authorId } = useParams()
   const [article, setArticle] = useState<Article | null>(null)
+  const [status, setStatus] = useState<string>('ok')
 
   useEffect(() => {
     const fetchArticle = async () => {
       try {
+        setStatus('pending')
         const data = await getArticleById(articleId as string)
         setArticle(data)
+        setStatus('ok')
       } catch (e) {
-        console.log(e)
+        setStatus('notfound')
       }
     }
     fetchArticle()
   }, [articleId])
 
-  if (!article) {
-    return <div>Loading...</div>
+  if (status === 'pending' && !article) {
+    return (
+      <>
+        <Box sx={{ display: 'flex', mb: 2 }}>
+          <Skeleton animation="wave" variant="circular" width={150} height={150} sx={{ mr: 2 }} />
+          <Box>
+            <Skeleton animation="wave" height={50} width={200} sx={{ mt: 4 }} />
+            <Skeleton animation="wave" height={50} width={100} />
+          </Box>
+        </Box>
+        <Typography variant="subtitle1" gutterBottom></Typography>
+      </>
+    )
   }
 
-  const shortWallet = shortenString(article.wallet)
+  if (status === 'notfound') {
+    return <NotFoundComponent />
+  }
 
   return (
     <>
-      <Container maxWidth="lg" sx={{ pt: 4 }}>
-        <SmallAvatar
-          to={`/profile/${authorId}`}
-          profile={{ name: article.name, avatar: article?.avatar, wallet: shortWallet }}
-        />
-        {article && <Output data={JSON.parse(article.content as string)} />}
-      </Container>
+      {article && (
+        <Container maxWidth="lg" sx={{ pt: 4 }}>
+          <SmallAvatar
+            to={`/profile/${authorId}`}
+            profile={{ name: article.name, avatar: article?.avatar, wallet: shortenString(article.wallet) }}
+          />
+          {article && <Output data={JSON.parse(article.content as string)} />}
+        </Container>
+      )}
     </>
   )
 }
