de913d3ab8b8a6ac401d01266bc393a7a3b5d827
igar1991 <iiggaarr19912004@gmail.com>
2023-07-23 20:36:00 +0300

    feat: login with fs

---
+++ src/utils/fs.ts
@@ -0,0 +1,91 @@
+import { Update, UpdateDataSigned } from '@fairjournal/file-system'
+
+export const PROJECT_NAME = 'fairjournal'
+
+/**
+ * Default directory for articles
+ */
+export const DEFAULT_DIRECTORY = 'articles'
+
+/**
+ * Status response from FS API
+ */
+export interface StatusResponse {
+  /**
+   * Status
+   */
+  status: string
+}
+
+/**
+ * User info from FS API - `/fs/user/info`
+ */
+interface UserInfo {
+  status: string
+  address: string
+  isUserExists: boolean
+}
+
+let update: Update | undefined
+
+/**
+ * Gets file system instance
+ */
+export function getUpdateInstance(userAddress: string): Update {
+  if (!update || update.getUserAddress() !== userAddress) {
+    update = new Update(PROJECT_NAME, userAddress)
+  }
+
+  return update
+}
+
+/**
+ * Clears file system instance
+ */
+export function clearUpdateInstance(): void {
+  update = undefined
+}
+
+/**
+ * Gets fs api url
+ *
+ * @param url Url
+ * @param params Query params
+ */
+function getFsApiUrl(url: string, params?: { [key: string]: string }): string {
+  const queryParams = params ? `?${new URLSearchParams(params).toString()}` : ''
+
+  return `${process.env.REACT_APP_URL_API}/fs/${url}${queryParams}`
+}
+
+/**
+ * Gets user info from FS API
+ *
+ * @param address User address
+ */
+export async function getUserInfo(address: string): Promise<UserInfo> {
+  const response = await fetch(getFsApiUrl('user/info', { address }))
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`)
+  }
+
+  return (await response.json()) as UserInfo
+}
+
+/**
+ * Applies update to the user file system
+ *
+ * @param update Update
+ */
+export async function updateApply(update: UpdateDataSigned): Promise<StatusResponse> {
+  return (await (
+    await fetch(getFsApiUrl('update/apply'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ update }),
+    })
+  ).json()) as StatusResponse
+}
